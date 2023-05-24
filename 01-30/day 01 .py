41778050a65ef4bce0ea653ebbca19246bba9ff8
Abdullah Al-Habal <dbnkalhbalb@gmail.com>
2023-05-25 00:46:01 +0300

    initial commit - we just started 25 May 2023

---
+++ 01-30/day 01 .py
@@ -0,0 +1,9 @@
+# 25 May 2023 12:42 AM , Damascus - Syria 
+
+# Hellow World!
+
+print("Helow World!")
+
+
+# in the terminal -- in the same directory type :  
+# python '.\day 01 .py\'                                     => Hello World!
